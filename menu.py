ba3adbcb3f7f42c209050d18a84222e6aafe3c00
ljouglar <mrtvfuencxozd>
2018-04-13 11:17:18 +0200

    Added docstrings. All messages turned to english.

--- menu.py
+++ menu.py
@@ -1,22 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import copy, curses
 from stack import Stack, QuitException, UnknownCommandException, StackUnderflowException
 
 class Menu:
+    "Show the calculator interface, handles user input"
+
     def __init__(self):
+        "Initialize an empty stack"
         self.stack = Stack()
         self.message_line = ""
 
     def get_input(self, prompt = "> "):
+        "Handles user input key by key"
         result = ""
         while True:
             self.stdscr.addstr(8, 0, "                       ")
             self.stdscr.addstr(8, 0, prompt + result)
+            curses.flushinp()
             key = self.stdscr.getch()
             if key == 27 or (chr(key) == "q" and result == ""):
                 result = "q"
                 break
-            elif key == 127:
+            elif key == 127 or key == 263:
                 if result == "":
                     result = "drop"
                     break
@@ -33,6 +40,7 @@ class Menu:
         return result
 
     def print_stack(self):
+        "Print the stack content on screen, plus the status line"
         for y in range(0, 7):
             try:
                 self.stdscr.addstr(7 - y, 0, str(self.stack[-y - 1]))
@@ -41,6 +49,7 @@ class Menu:
         self.stdscr.addstr(9, 0, self.message_line)
 
     def will_it_float(self, str):
+        "Indicate wether a string can be converted to float or not"
         try:
             float(str)
             return True
@@ -48,6 +57,7 @@ class Menu:
             return False
 
     def run(self):
+        "Handles user input wher the [enter] key is pressed"
         self.stdscr = curses.initscr()
         curses.noecho()
         curses.cbreak()
@@ -67,7 +77,7 @@ class Menu:
                     self.message_line = str(e)
                     self.stack = saved_stack
                 except ZeroDivisionError as e:
-                    self.message_line = "division par zéro"
+                    self.message_line = "division by zero"
                     self.stack = saved_stack
                 except UnknownCommandException as e:
                     self.message_line = str(e)
--- stack.py
+++ stack.py
@@ -1,14 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 class QuitException(BaseException):
+    "To send back the quit message"
     pass
 
 class UnknownCommandException(BaseException):
+    "To send back that the command is unknown"
     pass
 
 class StackUnderflowException(BaseException):
+    "To send back that ther is not enough elemnets in the stack to proceed the command"
     pass
 
 class Stack(list):
+    "Use the list class to simulate a RPN calculator stack"
+
     def __init__(self):
+        "Initialize the known commands and point to the according method"
         self._stack_commands = {
             "+": self.add, "add": self.add,
             "-": self.substract, "sub": self.substract, "subs": self.substract,
@@ -27,54 +36,66 @@ class Stack(list):
         }
 
     def execute_command(self, cmd):
+        "Execute the given command, sending back eventual errors"
         try:
             command = self._stack_commands[cmd]
         except:
-            raise UnknownCommandException("commande inconnue")
+            raise UnknownCommandException("unknown command")
         try:
             command()
         except IndexError as e:
-            raise StackUnderflowException("pas assez d'arguments")
+            raise StackUnderflowException("stack underflow")
 
     # Math methods
     def add(self):
+        "Adds last two stack elements"
         self.append(self.pop() + self.pop())
 
     def substract(self):
+        "Substracts last two stack elements"
         to_sub = self.pop()
         self.append(self.pop() - to_sub)
 
     def multiply(self):
+        "Multiplies last two stack elements"
         self.append(self.pop() * self.pop())
 
     def divide(self):
+        "Divides last two stack elements"
         to_div = self.pop()
         self.append(self.pop() / to_div)
 
     def power(self):
+        "Powers last two stack elements"
         to_power = self.pop()
         self.append(self.pop() ** to_power)
 
     # Stack manipulation
     def dup(self):
+        "Duplicates last stack element"
         to_dup = self.pop()
         self.append(to_dup)
         self.append(to_dup)
 
     def swap(self):
+        "Swaps last two stack elements"
         first = self.pop()
         second = self.pop()
         self.append(first)
         self.append(second)
 
     def drop(self):
+        "Removes last stack element"
         self.pop()
 
     def roll(self):
+        "Rolls the stack (first element becomes last)"
         self.insert(0, self.pop())
 
     def rolld(self):
+        "Rolls the stack (last element becomes first)"
         self.append(self.pop(0))
 
     def quit(self):
-        raise QuitException("sortie normale du programme")
+        "Send the quit exception to quit the calculator"
+        raise QuitException("exit by user")
